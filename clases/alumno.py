6dee5798557953b9687bf01b79c184a3e8d7d106
produgraf <produgraf2000@gmail.com>
2017-03-16 11:51:12 -0300

    agrego la clase alumno

---
+++ clases/alumno.py
@@ -0,0 +1,41 @@
+class Alumno(object)
+    nombre = ""
+    apellido = ""
+    fecha_Nac = ""
+    lista_Notas = []
+
+    def setNombre(self,n):
+        self.nombre = str(n)
+
+    def setApellido(self,a):
+        self.apellido = str(a)
+
+    def setFecha_Nac(self,fn):
+        self.fecha_Nac = str(fn)
+
+    def agregarNota(self,nota):
+        self.lista_Notas.append(nota)
+
+    def menorNota(self):
+        ahora = self.lista_Notas[0]
+        menor = ahora
+        for ahora in self.lista_Notas:
+            if ahora < menor:
+                menor = ahora
+        return menor
+
+    def mayorNota(self):
+        ahora = self.lista_Notas[0]
+        mayor = ahora
+        for ahora in self.lista_Notas:
+            if ahora > mayor:
+                menor = ahora
+        return mayor
+
+    def promedioNota(self):
+        total = 0
+        cantidad = 0
+        for ahora in self.lista_Notas:
+            cantidad = cantidad + ahora
+            total = total+1
+        return (total/cantidad)
