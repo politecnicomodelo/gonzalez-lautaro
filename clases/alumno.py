a4fab329e6c55f32e185088e5433a09a8720ad08
produgraf <produgraf2000@gmail.com>
2017-03-17 10:24:15 -0300

    agrego el promedio notas alumno

--- clases/alumno.py
+++ clases/alumno.py
@@ -1,10 +1,15 @@
 from datetime import date
 
+class Materia(object):
+    nombre = ""
+    lista_Notas = []
+
 class Alumno(object):
     nombre = ""
     apellido = ""
     fecha_Nac = date
     lista_Notas = []
+    misMaterias = []
 
     def setNombre(self,n):
         self.nombre = str(n)
@@ -15,14 +20,29 @@ class Alumno(object):
     def setFecha_Nac(self,fn):
         self.fecha_Nac = str(fn)
 
-    def agregarNota(self,nota):
-        self.lista_Notas.append(nota)
-
     def menorNota(self):
+        total = 0
+        for item in self.misMaterias:
+            total = total + sum(self.misMaterias[item].lista_Notas)
+        return total / len(self.misMaterias)
         return min(self.lista_Notas)
 
     def mayorNota(self):
         return max(self.lista_Notas)
 
-    def promedioNota(self):
-        return sum(self.lista_Notas)/len(self.lista_Notas)
+    def promedioNotaMateria(self,Nmateria):
+        return sum(self.misMaterias[Nmateria].lista_Notas)/len(self.misMaterias[Nmateria].lista_Notas)
+
+    def promedioNotaAlumno(self):
+        total = 0
+        for item in self.misMaterias:
+            total = total + sum(self.misMaterias[item].lista_Notas)
+        return total/len(self.misMaterias)
+
+    def agregarMateria(self,nb):
+        self.misMaterias.append(Materia)
+        self.misMaterias[(len(self.misMaterias))-1].nombre = str(nb)
+
+    def agregarNota(self,nota,Nmateria):
+        self.misMaterias[Nmateria].lista_Notas.append(nota)
+        print(self.misMaterias[Nmateria].lista_Notas[0])
